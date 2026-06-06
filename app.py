9d390c7d817f091abb2df2003670df8d3dbfb231
abdel13hammadi-ops <abdel13.hammadi@gmail.com>
2026-06-06 12:11:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -343,9 +343,8 @@ elif not st.session_state.submitted:
     )
 
 nav_cols = st.sidebar.columns(2)
-
     for i in range(len(questions)):
-     label = f"{i + 1}"
+    label = f"{i + 1}"
 
     if i in st.session_state.answers:
         label += "✓"
